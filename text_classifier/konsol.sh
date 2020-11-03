2559daa72a205b02cb8d33c47e68b8c4750dd503
Mohamed Kaba Diabakhate <mohamed.diabakhate@gmail.com>
2020-11-03 14:03:08 +0100

    Init Rainbow history

--- text_classifier/konsol.sh
+++ text_classifier/konsol.sh
@@ -4,3 +4,6 @@ path="/Users/py3/data science/digital_africa/Base_Challenge_1000/"
 find *.py "$path" | while read line; do
 	grep 'import' "$line"
 done
+
+echo "alias pip=/usr/local/bin/pip3" >> ~/.zshrc  
+echo "alias pip=/usr/local/bin/pip3" >> ~/.bashrc
